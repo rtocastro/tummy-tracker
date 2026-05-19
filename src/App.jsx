c54f14076a3f906df79b901e0f372c36bae893f7
rtocastro <ricktorres@live.com>
2026-05-18 21:49:59 -0700

    created activityfeedjsx and styled imported to app

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,42 @@ import "./index.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import PetCard from "./components/PetCard";
+import ActivityFeed from "./components/ActivityFeed";
 
 import { pets } from "./data/pets";
 
 function App() {
   return (
-    <main className="app-shell">
+  <main className="app-shell">
 
-      <Navbar />
+    <Navbar />
 
-      <div className="content-container">
+    <div className="content-container">
 
-        <Hero />
+      <div className="dashboard-layout">
 
-        <section className="pet-grid">
-          {pets.map((pet) => (
-            <PetCard
-              key={pet.id}
-              pet={pet}
-            />
-          ))}
+        <section>
+
+          <Hero />
+
+          <section className="pet-grid">
+            {pets.map((pet) => (
+              <PetCard
+                key={pet.id}
+                pet={pet}
+              />
+            ))}
+          </section>
+
         </section>
 
+        <ActivityFeed />
+
       </div>
-    </main>
-  );
+
+    </div>
+  </main>
+);
 }
 
 export default App;
