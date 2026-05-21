31b921a507e8527b16d2a5723896f1af540003a4
rtocastro <ricktorres@live.com>
2026-05-20 22:20:31 -0700

    updated firebasejs to take auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getFirestore} from "firebase/firestore"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  getFirestore,
+} from "firebase/firestore";
 
-// Your web app's Firebase configuration
+import {
+  getAuth,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBcZtUwQnPLY715s90c9pG__tbq3dh7uAM",
   authDomain: "tummy-tracker-c3c0f.firebaseapp.com",
@@ -14,7 +17,8 @@ const firebaseConfig = {
   appId: "1:137224100566:web:32a24d413fd985fac84777"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
